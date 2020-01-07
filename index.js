a01c5fd00479caf262126f88732939b3b011e56e
Cameron DeCoster <cameron.decoster@gmail.com>
2020-01-07 16:58:48 -0700

    program now working, added remaining states

--- index.js
+++ index.js
@@ -3,51 +3,69 @@
 const repoUrl = 'https://developer.nps.gov/api/v1/parks';
 const apiKey = 'Tm1KITEL3OVCt69MilJucLy5tIuzuiSQxfVjsjgY';
 
+// Generate park address from address parameters (line1, line2, etc.)
+function generateAddress(addressInfo) {
+    console.log('`generateAddress` ran');
+
+    let address = "";
+    address += (addressInfo.line1 !== "") ? addressInfo.line1 + '<br>' : '';
+    address += (addressInfo.line2 !== "") ? addressInfo.line2 + '<br>' : '';
+    address += (addressInfo.line3 !== "") ? addressInfo.line3 + '<br>' : '';
+    address += `${addressInfo.city}, ${addressInfo.stateCode} ${addressInfo.postalCode}`;
+    return address;
+}
+
+// Take results from API and display them in the container in the DOM
 function displayResults(results) {
     console.log('`displayResults` ran');
 
     // Clear out previous results
     $('#js-results-list').empty();
-
-    // Only add results if user has repos
-    if (results.length > 0) {
-        // For each item in results, extract name and repo URL,
-        // create string with that info, add that info to results
-        // list in DOM
-        for (let i = 0; i < results.length; i++) {
+    
+    // Only add results if parks were found (parks list length > 0)
+    if (parseInt(results.data.length) > 0) {
+        // For each item in results, extract name, description, URL, and address;
+        // create string with that info, add that info to results list in DOM        
+        for (let i = 0; i < results.data.length; i++) {
+            // Find address index for physical address if address exists
+            const addressIndex = (results.data[i].addresses.length > 0) ? results.data[i].addresses.findIndex(item => item.type === 'Physical') : -1;
+            
+            // Add park info as list item
             $('#js-results-list').append(`
                 <li>
-                    ${results[i].data.fullName}
+                    ${results.data[i].fullName}
                     <br>
-                    ${results[i].data.description}
                     <br>
-                    <a href="${results[i].data.url}">${results[i].data.url}</a>
+                    ${results.data[i].description}
+                    <br>
+                    <br>
+                    ${(addressIndex !== -1) ? generateAddress(results.data[i].addresses[addressIndex]) : 'No address listed'}
+                    <br><br>
+                    <a href="${results.data[i].url}">${results.data[i].url}</a>
+                    <br><br>
                 </li>`
             );
         }
     }
-    // Otherwise state no repos exist
+    // Otherwise state that no parks exist
     else {
-        $('#js-results-list').append('<li>User has no public repositories</li>');
+        $('#js-results-list').append('<li>There are no parks in the selected states</li>');
     }
-    // Change class to display results list
+    // Change classes to display results list
     $('#js-results').removeClass('hidden'); 
 }
 
+// Generate query string that concatenates all parameters in proper format
 function formatQueryParameters(queryParameters) {
-    console.log(queryParameters);
+    console.log('`formatQueryParameters` ran');
+
     const parameterString = Object.keys(queryParameters)
-        // .map(key => `${key}=${queryParameters[key]}`);
         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParameters[key])}`);
     console.log('parameterString = ' + parameterString);
     return parameterString.join('&');
 }
-// function formatQueryParams(params) {
-//     const queryItems = Object.keys(params)
-//       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-//     return queryItems.join('&');
-// }
 
+// Call NPS API to get list of parks given states and max results
 function getParks(statesList, maxResults) {
     console.log('`getRepos` ran');
 
@@ -59,28 +77,28 @@ function getParks(statesList, maxResults) {
 
     // Create parameters for API query
     const queryParameters = {
-        parkCode: 'acad',
         stateCode: statesList.join(','),
-        limit: maxResults
+        limit: maxResults,
+        fields: 'addresses',
+        api_key: apiKey
     }
-    console.log('stateCode = ' + queryParameters.stateCode);
     const parameterString = formatQueryParameters(queryParameters);
 
     // Create API call URL
     const url = repoUrl + '?' + parameterString;
-    console.log(url);
 
-    // Create fetch options for API authentication
-    const options = {
-        headers: new Headers({
-          "X-Api-Key": apiKey})
-    };
-    console.log(options);
+    // Create fetch options for API authentication > THIS DOESN"T WORK FOR SOME REASON
+    // const options = {
+    //     headers: new Headers({
+    //         'x-api-key': apiKey
+    //     })
+    // };
+    // console.log(options.headers.get('x-api-key'));
 
     // Call API
-    fetch(url, options)
-        // Do I need to handle a user with no repos?
+    fetch(url)
         .then(response => {
+            // Make sure response is good before proceeding, otherwise throw error
             if (response.ok) {
                 return response.json();
             }
@@ -88,7 +106,10 @@ function getParks(statesList, maxResults) {
                 throw new Error(response.statusText);
             }
         })
-        .then(responseJson => displayResults(responseJson))
+        .then(responseJson => {
+            console.log(responseJson);
+            displayResults(responseJson);
+        })
         .catch(err => {
             $('#js-error-message').text(`There was an error: ${err.message}`)
         });
@@ -101,10 +122,10 @@ function watchForm() {
     $('#js-form').submit(function () {
         event.preventDefault();
 
-        // Grab username from input field
+        // Grab states and max results from inputs
         const statesList = $('#js-park-states').val();
         const maxResults = $('#js-max-results').val();
-        console.log(statesList, maxResults);
+        
         getParks(statesList, maxResults);
 
     })
